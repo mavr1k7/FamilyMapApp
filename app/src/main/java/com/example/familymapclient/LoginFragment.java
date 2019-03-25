07eb4b0ee5f31bc6baedb2b7b5edce08730fd74d
teran <petersonteran@gmail.com>
2019-03-25 00:31:55 -0600

    Rebuilt the UI

---
+++ app/src/main/java/com/example/familymapclient/LoginFragment.java
@@ -0,0 +1,20 @@
+package com.example.familymapclient;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class LoginFragment extends Fragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_login, container, false);
+    }
+}
