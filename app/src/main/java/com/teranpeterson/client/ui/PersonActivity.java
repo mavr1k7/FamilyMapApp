6e632b178ec20f5c33e753f1be85f08feba1c7a2
teran <5717131+Dude9303@users.noreply.github.com>
2019-04-09 19:17:29 -0600

    Created person activity

---
+++ app/src/main/java/com/teranpeterson/client/ui/PersonActivity.java
@@ -0,0 +1,15 @@
+package com.teranpeterson.client.ui;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.teranpeterson.client.R;
+
+public class PersonActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_person);
+    }
+}
